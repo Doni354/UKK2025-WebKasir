27329c572bcb93aca52ca362fa8a3aeb6945a1fb
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2025-01-27 13:50:46 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,23 +40,23 @@
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
-                                    <div class="text-center">
+                                    <div class="text-center" method="POST">
                                         <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                     </div>
-                                    <form class="user">
+                                    <form action="/login" class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
+                                            <input type="email" name="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
                                                 placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
+                                            <input name="password" type="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Password">
                                         </div>
                                         <hr>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <button type="submit" href="index.html" class="btn btn-primary btn-user btn-block">
                                             Login
-                                        </a>
+                                        </button>
 
 
                                     </form>
